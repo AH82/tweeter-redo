57ac85c8953f8c5a5475a8712d6a12ffd08720d2
Hatem <hatem.deux@gmail.com>
2024-10-30 00:24:19 -0400

    feat: Implement character counter with warning if exceeded

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,3 +1,16 @@
 $(document).ready(function() {
 
+  $("textarea").on("input", function () {
+    const CHAR_LIMIT = 140;
+
+    let TextCurrentLength = CHAR_LIMIT - $(this).val().length;
+    const counter = $(this).parent().find("output.counter");
+    const counterWarning = "counter-warning";
+
+    $(counter).val(TextCurrentLength);
+    TextCurrentLength < 0 ? 
+      $(counter).addClass(counterWarning) 
+      : $(counter).removeClass(counterWarning);
+  })
+
 });
