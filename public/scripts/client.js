9ad4fd594f0f9d693588b717b1bbff082fa6c3da
Hatem <hatem.deux@gmail.com>
2024-11-01 15:17:50 -0400

    init createTweetElement(),  renderTweets(), mock data, JSdoc desc.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,41 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+
+// Test / driver code (temporary). Eventually will get this from the server.
+const tweetData = {
+  "user": {
+    "name": "Newton",
+    "avatars": "https://i.imgur.com/73hZDYK.png",
+    "handle": "@SirIsaac"
+    },
+  "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+  "created_at": 1461116232227
+}
+
+/**
+ * @summary turns tweet data into a tweet DOM element.
+ * @param {object} tweet - the tweet data object
+ * @returns {string} - returns a string representing a tweet elemnt  
+ */
+const createTweetElement = function (tweet) {
+  // code for creating the tweet element
+
+}; 
+
+/**
+ * @summary inserts/appends the list of tweet DOM elemnts, 
+ * @param {object[]} tweets - Array of objects representing tweets' data.
+ * 
+ * @description 
+  * inserts/appends the list of tweet DOM elemnts, 
+  * created by @see createTweetElement into {@link ../public/index.html}'s $section.tweet-container
+  * no return --> Appends a DOM element 
+ */
+const renderTweets = function (tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+};
