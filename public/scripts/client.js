d43a3b1d46d0b9c4bf233553bd43bbc5ba89279d
Hatem <hatem.deux@gmail.com>
2024-11-04 20:17:19 -0500

    delete hardcoded test drivers

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -58,43 +58,12 @@ return $tweet
   * @todo [x] takes return value and appends it to the tweets container
  */
 const renderTweets = function (tweets) {
+  console.log("rendering tweets ....")
   tweets.forEach($tweet => 
     $('section.tweet-container').append(createTweetElement($tweet)));
 };
 
-
-// Test / driver code (temporary). Eventually will get this from the server.
-// temp test data from server/data-files/initial-tweets.json
-let data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd" },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(function(){
-
-  console.log("rendering tweets ....")
-  renderTweets(data);
 
   /**
    * @description New Tweet <form> POST request through AJAX, preventing default
